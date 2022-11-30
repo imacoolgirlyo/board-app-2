a31f68e8e116f9d450b038b9a8d92d5d081728af
imacoolgirlyo <tjrud1438@naver.com>
2022-11-30 20:53:35 +0900

    feat: add TypeOrmModule.forRoot

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [
+    BoardsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'board-postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
