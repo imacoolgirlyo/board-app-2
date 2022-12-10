149a2a126a8452d1fd22865c38afc281862cea0f
imacoolgirlyo <tjrud1438@naver.com>
2022-12-10 14:13:26 +0900

    feat: fetch api to get boards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/boards", {
+      method: "get",
+    })
+      .then((response) => response.json())
+      .then((data) => setBoards(data));
+  }, []);
+
   return <div className="App">hello</div>;
 }
 
