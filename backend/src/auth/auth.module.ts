1b47b8dfde7ced8faf84fbf5914b60b2c392cf43
imacoolgirlyo <tjrud1438@naver.com>
2022-12-13 15:08:15 +0900

    feat: google oauth guard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GoogleAuthController } from './google.auth.controller';
 import { GoogleAuthService } from './google.auth.service';
+import { GoogleOauthGuard } from './google.oauth.guard';
 import { GoogleStrategy } from './google.strategy';
 
 @Module({
   imports: [],
   controllers: [GoogleAuthController],
-  providers: [GoogleAuthService, GoogleStrategy],
+  providers: [GoogleAuthService, GoogleStrategy, GoogleOauthGuard],
 })
 export class AuthModule {}
--- backend/src/auth/google.auth.controller.ts
+++ backend/src/auth/google.auth.controller.ts
@@ -1,19 +1,20 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { GoogleAuthService } from './google.auth.service';
+import { GoogleOauthGuard } from './google.oauth.guard';
 
-@Controller('google')
+@Controller('auth/google')
 export class GoogleAuthController {
   constructor(private readonly googleAuthService: GoogleAuthService) {}
 
   @Get()
-  @UseGuards(AuthGuard('google'))
+  @UseGuards(GoogleOauthGuard)
   async googleAuth(@Req() req) {
-    //
+    // Guard가 Redirect 함
   }
 
   @Get('redirect')
-  @UseGuards(AuthGuard('google'))
+  @UseGuards(GoogleOauthGuard)
   googleAuthRedirect(@Req() req) {
     return this.googleAuthService.googleLogin(req);
   }
