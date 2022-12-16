63acfd18b5a5ae45f3080aca08f9e85cb7901584
imacoolgirlyo <tjrud1438@naver.com>
2022-12-16 18:29:09 +0900

    feat: add /boards router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/boards", element: <div>??</div> },
+]);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
