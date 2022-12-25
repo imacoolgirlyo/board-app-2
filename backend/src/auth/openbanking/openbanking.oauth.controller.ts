52520f5e48575d4eec63aa3cd082182a3d1592f2
imacoolgirlyo <tjrud1438@naver.com>
2022-12-25 19:03:52 +0900

    feat: get user data and token by requesting code at openbanking/oauth/token

--- backend/src/auth/openbanking/openbanking.oauth.controller.ts
+++ backend/src/auth/openbanking/openbanking.oauth.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
-import { firstValueFrom } from 'rxjs';
-import * as FormData from 'form-data';
+import { Response } from 'express';
+
+interface OpenBankingUserData {
+  access_token: string;
+  token_type: string;
+  refresh_token: string;
+  expires_in: number;
+  scope: string;
+  user_seq_no: string;
+}
 
 @Controller('auth/open-banking')
 export class OpenBankingOAuthController {
@@ -12,39 +20,35 @@ export class OpenBankingOAuthController {
   ) {}
 
   @Get()
-  async openBankingAuth(@Query() query: { code: string }) {
-    console.log(query.code);
+  async getAccessToken(
+    @Req() req,
+    @Res() response: Response,
+    @Query() query: { code: string },
+  ) {
     const openBankingUrl = this.configService.get<string>(
       'OPEN_BANKING_TEST_API',
     );
 
-    const formData = new FormData();
-    formData.append('code', query.code);
-    formData.append(
-      'client_id',
-      this.configService.get<string>('OPEN_BANKING_CLINET_ID'),
-    );
-    formData.append(
-      'client_secret',
-      this.configService.get<string>('OPEN_BANKING_SECERT'),
-    );
-    formData.append('redirect_uri', 'http://localhost:3000/login');
-    formData.append('grant_type', 'authorization_code');
-
     const data = {
       code: query.code,
-      client_id: this.configService.get<string>('OPEN_BANKING_CLINET_ID'),
-      client_secret: this.configService.get<string>('OPEN_BANKING_SECERT'),
-      redirect_uri: 'http://localhost:3000/login',
+      client_id: 'c60b06fb-1c0f-4d58-8d28-6fe4cca77f22',
+      client_secret: '0f481f5f-7177-4d0b-a5ed-fbda542a07ba',
+      redirect_uri: 'http://localhost:5000/auth/open-banking',
       grant_type: 'authorization_code',
     };
 
-    const response = await firstValueFrom(
-      this.httpService.post(openBankingUrl, JSON.stringify(data), {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      }),
+    const res = await this.httpService.axiosRef.post<OpenBankingUserData>(
+      `${openBankingUrl}/oauth/2.0/token`,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        },
+      },
     );
 
-    console.log(response);
+    return response.redirect(
+      `http://localhost:3000/login?open_b_userId=${res.data.user_seq_no}`,
+    );
   }
 }
