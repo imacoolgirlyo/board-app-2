79334d2b76b3bedda0cadae5487786b23a6b7ced
imacoolgirlyo <tjrud1438@naver.com>
2023-01-02 16:01:42 +0900

    refactor: validate in googleStrategy use convertToOAuthProfile

--- backend/src/auth/authProfile.ts
+++ backend/src/auth/authProfile.ts
@@ -17,6 +17,27 @@ export interface IFacebookProfile {
   _raw: string;
 }
 
+export interface IGoogleProfile {
+  id: string;
+  displayName: string;
+  provider: string; // 'google'
+  name?: { familyName: string; givenName: string };
+  emails?: { value: string; verified: boolean }[];
+  photos?: { value: string }[];
+  _raw: string;
+  _json: {
+    sub: string;
+    name?: string;
+    given_name?: string;
+    family_name?: string;
+    picture?: string;
+    email?: string;
+    email_verified?: boolean;
+    locale?: string;
+    hd?: string;
+  };
+}
+
 abstract class AuthProfile {
   profile: any;
   constructor(profile: any) {
@@ -71,5 +92,48 @@ export class FacebookProfile extends AuthProfile {
       name: this.getName(),
       photo: this.getPhotoUrl(),
     };
+  }
+}
+
+export class GoogleProfile extends AuthProfile {
+  profile: IGoogleProfile;
+  constructor(profile: IGoogleProfile) {
+    super(profile);
+    this.profile = profile;
+  }
+
+  public convertToOAuthProfile(
+    accessToken: string,
+    refreshToken?: string,
+  ): OAuthProfile {
+    return {
+      localId: this.profile.id,
+      provider: IdentityProvider.GOOGLE,
+      accessToken,
+      refreshToken,
+      email: this.getEmail(),
+      name: this.getName(),
+      photo: this.getPhotoUrl(),
+    };
+  }
+
+  getPhotoUrl() {
+    return this.profile.photos.length > 0
+      ? this.profile.photos[0].value
+      : undefined;
+  }
+
+  getName() {
+    return this.profile.name
+      ? `${this.profile.name.familyName ?? ''} ${
+          this.profile.name.givenName ?? ''
+        }`
+      : undefined;
+  }
+
+  getEmail() {
+    return this.profile.emails.length > 0
+      ? this.profile.emails[0].value
+      : undefined;
   }
 }
--- backend/src/auth/google.strategy.ts
+++ backend/src/auth/google.strategy.ts
@@ -3,29 +3,8 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-google-oauth20';
 import { ConfigService } from '@nestjs/config';
 import { UserService } from 'src/user/user.service';
-import { IdentityProvider } from './socialProfile.model';
 import { IUser } from 'src/user/user.model';
-
-interface GoogleProfile {
-  id: string;
-  displayName: string;
-  provider: string; // 'google'
-  name?: { familyName: string; givenName: string };
-  emails?: { value: string; verified: boolean }[];
-  photos?: { value: string }[];
-  _raw: string;
-  _json: {
-    sub: string;
-    name?: string;
-    given_name?: string;
-    family_name?: string;
-    picture?: string;
-    email?: string;
-    email_verified?: boolean;
-    locale?: string;
-    hd?: string;
-  };
-}
+import { GoogleProfile, IGoogleProfile } from './authProfile';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
@@ -41,17 +20,13 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
   async validate(
     accessToken: string,
     refreshToken: string,
-    profile: GoogleProfile,
+    profile: IGoogleProfile,
   ): Promise<IUser> {
-    const user = await this.userService._findOrCreate({
-      localId: profile.id,
-      provider: IdentityProvider.GOOGLE,
-      accessToken,
-      refreshToken,
-      email: profile.emails[0].value,
-      name: `${profile.name.familyName ?? ''} ${profile.name.givenName ?? ''}`,
-      photo: profile.photos[0].value,
-    });
+    const googleProfile = new GoogleProfile(profile);
+
+    const user = await this.userService._findOrCreate(
+      googleProfile.convertToOAuthProfile(accessToken, refreshToken),
+    );
 
     return user;
   }
